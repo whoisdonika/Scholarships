ac4bc2bddfc131f65b1327df338428de5ab089b8
Admin <Admin@DESKTOP-4RHGVT9>
2024-11-07 22:37:21 +0200

    Add project files.

---
+++ Scholarships/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholarships
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Scholarship example = new Scholarship("academic", 35);
+                example.PrintExample();
+
+                Console.WriteLine("--------------------------");
+
+                List<Scholarship> studentList = new List<Scholarship>();
+                List<Scholarship> goodGrades = new List<Scholarship>();
+
+                Console.Write("How many students would you like to add? - ");
+                int n = int.Parse(Console.ReadLine());
+
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Enter all the information :  ");
+                    string[] input = Console.ReadLine().Split(' ');
+
+                    Scholarship scholarship = new Scholarship(input[0], double.Parse(input[1]), input[2], double.Parse(input[3]), input[4]);
+                    studentList.Add(scholarship);
+
+                    if (double.Parse(input[3]) > 4.5)
+                    {
+                        goodGrades.Add(scholarship);
+                    }
+
+
+                    Console.WriteLine("------------------------");
+                }
+
+
+                Console.WriteLine("All students : ");
+                foreach (Scholarship student in studentList)
+                {
+                    student.PrintAllInfo();
+                }
+
+                Console.WriteLine("All students with grades above 4,50 : ");
+                foreach (Scholarship student in goodGrades)
+                {
+                    student.PrintAllInfo();
+                }
+
+                Console.WriteLine("Student with the highest grade : ");
+                studentList.OrderByDescending(x => x.Grade);
+                studentList[0].PrintAllInfo();
+
+                Console.WriteLine("Students sorted by name (ascending) : ");
+                studentList.OrderBy(x => x.Name);
+
+                foreach (Scholarship student in studentList)
+                {
+                    student.PrintAllInfo();
+                }
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Scholarships/Scholarship.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholarships
+{
+    public class Scholarship
+    {
+
+		
+		private string type;
+
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		private double sum;
+
+		public double Sum
+		{
+			get { return sum; }
+			set { sum = value; }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private double grade;
+
+		public double Grade
+		{
+			get { return grade; }
+			set
+			{
+				if (value > 6 || value < 2)
+				{
+					throw new ArgumentOutOfRangeException( "Grade must be between 2 and 6");
+				}
+				grade = value;
+				
+			}
+		}
+
+		private string detentions;
+
+		public string Detentions
+		{
+			get { return detentions; }
+			set { detentions = value; }
+		}
+
+        public Scholarship(string type, double sum, string name, double grade, string detentions)
+        {
+			this.type = type;
+			this.sum = sum;
+			this.name = name;
+			this.grade = grade;
+			this.detentions = detentions;
+        }
+
+        public Scholarship(string type, double sum)
+        {
+            this.type = type;
+            this.sum = sum;
+        }
+
+		public void PrintExample()
+		{
+			Console.WriteLine($"Scholarship type : {this.type} \n Scholarship sum : {this.sum}");
+		}
+
+        public void PrintAllInfo()
+        {
+            Console.WriteLine($"Scholarship type : {this.type} \nScholarship sum : {this.sum}" +
+				$" \nStudent name : {this.name} \nStudent grade : {this.grade} \nDetentions : {this.detentions} \n---------------------");
+        }
+
+		
+
+
+    }
+}
